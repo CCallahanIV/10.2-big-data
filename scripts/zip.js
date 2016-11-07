6fd11b22dbe59f42ccbd63794166049c5c9a5c16
Ted Callahan <CCallahanIV@gmail.com>
2016-11-07 10:16:34 -0800

    Data analysis completed

--- scripts/zip.js
+++ scripts/zip.js
@@ -1,12 +1,88 @@
 (function(module) {
   var zip = {};
+  var zips = [];
 
   getData = function() {
     $.getJSON('/data/manhattan.json', function(data) {
-      // TODO: start here!
+      zips = data.features;
+      console.log('loaded');
+
+      var newZips = trimData(zips);
+      console.log(newZips);
+
+      var topFiveNeighborhoods = topFive(newZips);
+      console.log(topFiveNeighborhoods);
+
+    //END OF .getJSON
     });
   };
+
+  function topFive(newZips){
+    return newZips.map(function(ele){
+      return ele.neighborhood;
+    }).reduce(function(uniqueNeighbors, curNeighbor) {
+      if (uniqueNeighbors.indexOf(curNeighbor) === -1) {
+        uniqueNeighbors.push(curNeighbor);
+      }
+      return uniqueNeighbors;
+    }, []).map(function(neighborhood){
+      return {
+        'neighborhood': neighborhood,
+        'numZips': newZips.filter(function(ele){
+          return ele.neighborhood === neighborhood;
+        }).length
+      }
+    }).sort(function(a,b){
+      return b.numZips - a.numZips;
+    }).slice(0,5);
+  };
+
+  function trimData(data){
+    return data.map(function(ele){
+      return {
+        'zip': ele.properties.zip,
+        'neighborhood': ele.properties.neighborhood,
+        'address': checkNeighborhood(ele),
+        'coordinates': {
+          'lat': ele.geometry.coordinates[0],
+          'lng': ele.geometry.coordinates[1]
+        }
+      };
+    });
+
+    function checkNeighborhood(ele){
+      if (ele.properties.neighborhood === ""){
+        return null;
+      } else {
+        return ele.properties.neighborhood;
+      };
+    };
+
+  };
+
 
   getData();
   module.zip = zip;
 })(window);
+
+
+// data.features.forEach(function(ele){
+//   zip.zips.push(
+//     zip[ele.properties.zip] = {
+//     'neighborhood': ele.properties.neighborhood,
+//     'address':
+//       function(){
+//         if (ele.properties.neighborhood === ""){
+//           return null;
+//         } else {
+//           return ele.properties.neighborhood;
+//         };
+//       },
+//     'coordinates': {
+//       'lat': ele.geometry.coordinates[0],
+//       'lng': ele.geometry.coordinates[1]
+//     }
+//   }
+// );
+// });
+// )};
